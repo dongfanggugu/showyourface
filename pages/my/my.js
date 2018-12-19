08b83e38adc1750679204e58d97f636182cd638b
刘松伟 <liusongwei@imzhiliao.com>
2018-12-19 12:31:40 +0800

    my page

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,45 +1,38 @@
-//index.js
-//获取应用实例
+// pages/my/my.js
+
 const app = getApp()
 
 Page({
+
+ 
+  /**
+   * 页面的初始数据
+   */
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  getCarInfo: function() {
-    var that = this;
-    wx.request({
-      url: 'https://www.showyourface.com/cars',
-      method: 'GET',
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        console.log(res.data)
-        var cars = res.data[0].desc;
-        that.setData({
-          mycar: cars
-        })
-      }
-    })
-  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function () {
-    this.getCarInfo();
+    
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -61,14 +54,53 @@ Page({
       })
     }
   },
-  goNextPage: function() {
-    wx.navigateTo({
-      url: '/pages/tabbar/tabbar'
-    });
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
-  goMy: function() {
-    wx.navigateTo({
-      url: '/pages/my/my',
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
