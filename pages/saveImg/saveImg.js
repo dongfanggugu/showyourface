53aa9410539b0ec6c95b271180c0217dc9f5fe80
刘松伟 <liusongwei@imzhiliao.com>
2018-12-19 13:18:18 +0800

    save image

--- pages/saveImg/saveImg.js
+++ pages/saveImg/saveImg.js
@@ -62,5 +62,30 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  saveImageToLocal: function (){
+    console.log("save image to local")
+  
+    wx.saveImageToPhotosAlbum({
+      filePath: './testSave.jpeg',
+      success(res) {
+        wx.showToast({
+          title: '保存图片成功',
+        })
+      },
+      fail(res) {
+        console.log("fail" + res.data)
+        wx.showToast({
+          title: '保存图片失败',
+        })
+      }
+    })
+
+    console.log("save image to local finished")
+  },
+
+  share: function (){
+    console.log(" share image")
   }
 })
