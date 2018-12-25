53d2991d7746bb8e0246d71e0daf8f25d1c1ee73
horizonrh <horizonrh@gmail.com>
2018-12-25 19:20:34 +0800

    用户登录

--- pages/index/index.js
+++ pages/index/index.js
@@ -32,8 +32,78 @@ Page({
       }
     })
   },
+
+  goLogin: function () {
+    wx.login({
+      success(res) {
+        if (res.code) {
+          console.log('code:' + res.code)
+          // 发起网络请求
+          wx.request({
+            url: 'http://www.skinrec.com:33333',
+            data: {
+              code: res.code
+            },
+            header: {
+              "content-type": "application/json"
+            },
+            method: 'GET',
+            success: function (res) {
+              console.log(res)
+              wx.setStorage({
+                //后面写完再回来改
+                key: "thirdSession",
+                data: res.data,
+              })
+            },
+            fail: function (res) {
+              console.log(res);
+            },
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
+  getUserInfoOld: function () {
+    //this.getCarInfo();
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+
+  bindGetUserInfo: function (e) {
+    var that = this;
+    console.log(e.detail.userInfo);
+  },
+
   onLoad: function () {
-    this.getCarInfo();
+    //this.getCarInfo();
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
